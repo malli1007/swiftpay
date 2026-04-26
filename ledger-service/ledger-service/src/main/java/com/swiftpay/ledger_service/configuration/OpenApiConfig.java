fe29573b32d06c8922649b01945387581861b048
malli1007 <mallikrishna217@gmail.com>
2026-04-26 17:57:43 +0530

    test

--- ledger-service/ledger-service/src/main/java/com/swiftpay/ledger_service/configuration/OpenApiConfig.java
+++ ledger-service/ledger-service/src/main/java/com/swiftpay/ledger_service/configuration/OpenApiConfig.java
@@ -14,6 +14,6 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("SwiftPay API")
                         .version("1.0")
-                        .description("Ledger Microservice APIs"));
+                        .description("Ledger Microservice APIs test"));
     }
 }
