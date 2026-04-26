852179df9e75db553af98283f25d6af39cd73a2e
malli1007 <mallikrishna217@gmail.com>
2026-04-26 17:41:20 +0530

    add actuator and swagger

--- transaction-gateway-service/transaction-gateway-service/src/main/java/com/swiftpay/transaction_gateway_service/controller/PaymentController.java
+++ transaction-gateway-service/transaction-gateway-service/src/main/java/com/swiftpay/transaction_gateway_service/controller/PaymentController.java
@@ -1,12 +1,11 @@
 package com.swiftpay.transaction_gateway_service.controller;
 
-// ===============================
-// 6. PaymentController.java
-// ===============================
-
 import com.swiftpay.transaction_gateway_service.model.PaymentRequest;
 import com.swiftpay.transaction_gateway_service.model.PaymentResponse;
 import com.swiftpay.transaction_gateway_service.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +13,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/payments")
 @RequiredArgsConstructor
+@Tag(name = "Payments", description = "Payment APIs")
 public class PaymentController {
 
     private final PaymentService paymentService;
 
     @PostMapping
+    @Operation(
+            summary = "Create Payment",
+            description = "Creates a new payment transaction"
+    )
+    @ApiResponse(responseCode = "200", description = "Payment processed successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid request")
     public PaymentResponse createPayment(
             @Valid @RequestBody PaymentRequest request) {
 
-       return paymentService.processPayment(request);
+        return paymentService.processPayment(request);
     }
 }
